3c52054059f90a7270e12257890926cca957ed16
Kim R <kimberleyrangel1@gmail.com>
2020-05-30 14:55:50 -0400

    DeletePost

--- src/controllers/post_controller.js
+++ src/controllers/post_controller.js
@@ -55,7 +55,13 @@ export const getPost = (req, res) => {
 
 
 export const deletePost = (req, res) => {
-  res.send('delete a post here');
+  Post.findByIdAndDelete(req.params.id)
+  .then((result)=>{
+    res.send(result);
+  })
+  .catch((error) =>{
+    res.status(200).json({error});
+  });
 };
 // export const updatePost = (req, res) => {
 //   res.send('update a post here');
