16d653f5c494a91008e4a0d83b3f214dde5c9bad
Michael Zhou <michaelzhou01@gmail.com>
2020-06-01 23:04:28 -0500

    added auth to backend

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import * as Posts from './controllers/post_controller';
-
+import * as UserController from './controllers/user_controller';
+import { requireAuth, requireSignin } from './services/passport';
 
 const router = Router();
 
@@ -9,19 +10,32 @@ router.get('/', (req, res) => {
 });
 
 // /your routes will go here
-
 router.route('/tutors')
-  .post(Posts.createPost)
+  .post(requireAuth, (req, res) => {
+    Posts.createPost(req, res);
+  })
   .get(Posts.getTutorPosts);
 
 router.route('/tutees')
-  .post(Posts.createPost)
+  .post(requireAuth, (req, res) => {
+    Posts.createPost(req, res);
+  })
   .get(Posts.getTuteePosts);
 
+router.route('/tuteesGrouped')
+  .get(Posts.getTuteePostsGrouped);
+
+router.route('/tutorsGrouped')
+  .get(Posts.getTutorPostsGrouped);
 
 router.route('/posts/:id')
   .get(Posts.getPost)
-  .delete(Posts.deletePost);
+  .delete(requireAuth, (req, res) => {
+    Posts.deletePost(req, res);
+  });
 
+router.post('/signin', requireSignin, UserController.signin);
+
+router.post('/signup', UserController.signup);
 
 export default router;
